87653265191c07fd5bd943631f502f1fa7dd86f1
Queen_of_Undead <victorvsgaming@gmail.com>
2023-05-20 22:19:13 -0500

    capitalized the g in ghost

--- vite-project/projects.js
+++ vite-project/projects.js
@@ -1,7 +1,7 @@
 export default [
     {
-        Name: "Guess the ghost",
-        desc: "Guess the ghost is a game where a twitch streamer can play with their chat. Who ever guesses either the ghost room, ghost type, or both correctly they win a prize with the streamer's disgression",
+        Name: "Guess the Ghost",
+        desc: "Guess the Ghost is a game where a twitch streamer can play with their chat. Who ever guesses either the ghost room, ghost type, or both correctly they win a prize with the streamer's disgression",
         Link: "https://github.com/QueenofUndead/guesstheghost"
     },
     {
