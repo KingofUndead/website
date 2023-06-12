ff314d80e68fe0247493b522ca45e651d9aa8ac3
Queen_of_Undead <victorvsgaming@gmail.com>
2023-06-11 21:04:16 -0500

    fixed text

--- vite-project/projects.js
+++ vite-project/projects.js
@@ -1,7 +1,7 @@
 export default [
     {
         Name: "Guess the Ghost",
-        desc: "Guess the Ghost is a game where a twitch streamer can play with their chat. Who ever guesses either the ghost room, ghost type, or both correctly they win a prize with the streamer's disgression",
+        desc: "Guess the Ghost is a game a twitch streamer can play with their chat. The person who correctly guesses the ghost room and/or type wins a prize within the streamer's discretion.",
         Link: "https://github.com/QueenofUndead/guesstheghost"
     },
     {
